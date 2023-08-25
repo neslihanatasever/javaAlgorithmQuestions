9ba35e609814992b1da9cfc4dbd2c36eb76215a4
neslihanatever <neslihan_atasvr111@outlook.com>
2023-08-25 17:05:06 +0300

    armstrong numbers

---
+++ src/loops/ArmstrongNumbers.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class ArmstrongNumbers {
+    public static void main(String[] args) {
+        int a = 2451, basamakSayisi = 0;
+        }
+}
+//2451 / 10 = 245
+//245 / 10 = 24
+//24 / 10 = 2
+//2 / 10 = 0
+//önceliğimiz sayının basamak sayısını bulmak
